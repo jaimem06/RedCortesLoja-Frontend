7f8cdac141c6232eb73c12c9126ecc2e8b3d4eff
Alyce Maldonado <alyce.maldonado@unl.edu.ec>
2025-01-30 08:09:54 -0500

    Nueva actualizacion usuario, connection

--- src/components/Tables/TablePersona.tsx
+++ src/components/Tables/TablePersona.tsx
@@ -1,10 +1,7 @@
 import { useRouter } from "next/navigation";
-import { modificar_estado, listar_usuarios, eliminar_usuario } from "@/hooks/servicio_persona";
+import { listar_usuarios } from "@/hooks/servicio_persona";
 import { useEffect, useState } from "react";
-import swal from "sweetalert";
 import Cookies from "js-cookie";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPencilAlt, faTrash, faLock, faLockOpen } from '@fortawesome/free-solid-svg-icons';
 
 const TablePersona = () => {
   const ruta = useRouter();
@@ -33,197 +30,9 @@ const TablePersona = () => {
     obtener_usuarios();
   }, []);
 
-  // Función para redireccionar a editar usuario
-  function boton_click(external: string): void {
-    ruta.push("admin-usuario/" + external);
-  }
-
   // Función para redireccionar a registrar usuario
   function registrar_usuario() {
     ruta.push("admin-usuario/nuevo");
-  }
-
-  // Función para cambiar el estado de la cuenta del usuario
-  function cambiar_estado(external: string, estado: boolean) {
-    if (estado === true) {
-      swal({
-        title: "¿Está seguro de desactivar la cuenta?",
-        text: "Esta acción no se puede deshacer",
-        icon: "warning",
-        buttons: {
-          cancel: {
-            text: "Cancelar",
-            value: false,
-            visible: true,
-            className: "",
-            closeModal: true,
-          },
-          confirm: {
-            text: "Aceptar",
-            value: true,
-            visible: true,
-            className: "btn-danger",
-            closeModal: true,
-          },
-        },
-        dangerMode: true,
-      }).then((aceptar) => {
-        if (!aceptar) {
-          return;
-        } else {
-          console.log("Valor de external_id antes de llamar a modificar_estado:", external);
-          modificar_estado(external).then((res) => {
-            console.log("Respuesta de modificar estado:", res);
-  
-            // Verificamos si 'res' y 'res.datos' existen antes de acceder a 'error'
-            if (res && res.code === 200 && res.data) {
-              obtener_usuarios();
-              swal({
-                title: "Éxito",
-                text: res.data.tag || "Operación exitosa",
-                icon: "success",
-                button: "Aceptar",
-              });
-              cerrar_sesion(external);
-            } else {
-              // Si 'res.datos' no está definido, mostramos un mensaje de error general
-              console.log("Error en la respuesta de modificar_estado:", res);
-              swal({
-                title: "Error",
-                text: res && res.datos ? res.datos.error : "Error desconocido", // Validamos si 'datos' está definido
-                icon: "error",
-                button: "Aceptar",
-              });
-            }
-          }).catch(error => {
-            // Captura cualquier error en la llamada a modificar_estado
-            console.error("Error en la llamada a modificar_estado:", error);
-            swal({
-              title: "Error",
-              text: "Hubo un problema al realizar la operación.",
-              icon: "error",
-              button: "Aceptar",
-            });
-          });
-        }
-      });
-    } else {
-      swal({
-        title: "¿Está seguro de activar la cuenta?",
-        text: "Esta acción no se puede deshacer",
-        icon: "warning",
-        buttons: {
-          cancel: {
-            text: "Cancelar",
-            value: false,
-            visible: true,
-            className: "",
-            closeModal: true,
-          },
-          confirm: {
-            text: "Aceptar",
-            value: true,
-            visible: true,
-            className: "btn-success",
-            closeModal: true,
-          },
-        },
-      }).then((aceptar) => {
-        if (!aceptar) {
-          return;
-        } else {
-          modificar_estado(external).then((res) => {
-            console.log("Respuesta de modificar estado:", res);
-  
-            if (res && res.code === 200 && res.data) {
-              obtener_usuarios();
-              swal({
-                title: "Éxito",
-                text: res.data.tag || "Operación exitosa",
-                icon: "success",
-                button: "Aceptar",
-              });
-            } else {
-              swal({
-                title: "Error",
-                text: res && res.datos ? res.datos.error : "Error desconocido", // Validamos res.datos
-                icon: "error",
-                button: "Aceptar",
-              });
-            }
-          }).catch(error => {
-            console.error("Error en la llamada a modificar_estado:", error);
-            swal({
-              title: "Error",
-              text: "Hubo un problema al realizar la operación.",
-              icon: "error",
-              button: "Aceptar",
-            });
-          });
-        }
-      });
-    }
-  }
-  
-  
-
-
-  // Función para eliminar usuario
-  function eliminar(external: string) {
-    swal({
-      title: "¿Está seguro de eliminar este usuario?",
-      text: "Esta acción no se puede deshacer",
-      icon: "warning",
-      buttons: {
-        cancel: {
-          text: "Cancelar", // Nombre del botón de cancelar
-          value: false,
-          visible: true,
-          className: "",
-          closeModal: true,
-        },
-        confirm: {
-          text: "Aceptar", // Nombre del botón de aceptar
-          value: true,
-          visible: true,
-          className: "btn-danger",
-          closeModal: true,
-        },
-      },
-      dangerMode: true,
-    }).then((aceptar) => {
-      if (!aceptar) {
-        return;
-      } else {
-        eliminar_usuario(external).then((res) => {
-          if (res && res.code === 200) {
-            obtener_usuarios();
-            swal({
-              title: "Exito",
-              text: "Usuario eliminado correctamente.",
-              icon: "success",
-              button: "Aceptar",
-            });
-          } else {
-            swal({
-              title: "Error",
-              text: res.datos ? res.datos.error : "Error desconocido",
-              icon: "error",
-              button: "Aceptar",
-            });
-          }
-        });
-      }
-    });
-  }
-
-  function cerrar_sesion(external: String) {
-    if (ext === external) {
-      Cookies.remove("external");
-      Cookies.remove("token");
-      Cookies.remove("user");
-      ruta.push("/inicio-sesion");
-    }
   }
 
   return (
@@ -247,7 +56,8 @@ const TablePersona = () => {
               fillRule="evenodd"
               clipRule="evenodd"
               d="M10 1.66667C10.3682 1.66667 10.6667 1.96517 10.6667 2.33333V8.66667H16.6667C17.0349 8.66667 17.3333 8.96517 17.3333 9.33333C17.3333 9.7015 17.0349 10 16.6667 10H10.6667V16.6667C10.6667 17.0349 10.3682 17.3333 10 17.3333C9.63183 17.3333 9.33333 17.0349 9.33333 16.6667V10H3.33333C2.96517 10 2.66667 9.7015 2.66667 9.33333C2.66667 8.96517 2.96517 8.66667 3.33333 8.66667H9.33333V2.33333C9.33333 1.96517 9.63183 1.66667 10 1.66667Z"
-              fill=""/>
+              fill=""
+            />
           </svg>
         </button>
         <div
@@ -265,9 +75,6 @@ const TablePersona = () => {
                 </th>
                 <th className="min-w-[120px] px-4 py-4 font-medium text-dark dark:text-white">
                   Estado
-                </th>
-                <th className="px-4 py-4 text-right font-medium text-dark dark:text-white xl:pr-7.5">
-                  Acciones
                 </th>
               </tr>
             </thead>
@@ -299,31 +106,6 @@ const TablePersona = () => {
                       >
                         {persona.estado ? "Activo" : "Inactivo"}
                       </p>
-                    </td>
-
-                    <td
-                      className={`border-[#eee] px-4 py-4 text-right dark:border-dark-3 xl:pr-7.5 ${index === data.length - 1 ? "border-b-0" : "border-b"}`}
-                    >
-                      <div className="flex justify-end space-x-2">
-                        <button
-                          onClick={() => boton_click(persona.external)}
-                          className="text-primary hover:text-blue-500 dark:text-white dark:hover:text-gray-300"
-                        >
-                          <FontAwesomeIcon icon={faPencilAlt} />
-                        </button>
-                        <button
-                          onClick={() => eliminar(persona.external)}
-                          className="text-danger hover:text-red-500 dark:text-white dark:hover:text-gray-300"
-                        >
-                          <FontAwesomeIcon icon={faTrash} />
-                        </button>
-                        <button
-                          onClick={() => cambiar_estado(persona.external, persona.estado)}
-                          className={`text-${persona.estado ? "danger" : "success"} hover:text-${persona.estado ? "red" : "green"}-500`}
-                        >
-                          <FontAwesomeIcon icon={persona.estado ? faLock : faLockOpen} />
-                        </button>
-                      </div>
                     </td>
                   </tr>
                 ))}
--- src/hooks/connection.js
+++ src/hooks/connection.js
@@ -1,73 +1,61 @@
-const URL = 'http://localhost:5000/';
-//const URL = 'https://serviciodeusuariosyubicaciones-ejc7emcfe4atbvde.canadacentral-01.azurewebsites.net/';
+const URL = 'http://127.0.0.1:5000/';
 const URLU = 'https://servicio-dpdubkhed9bac2dk.eastus-01.azurewebsites.net/';
-//URLU para micrservicio de supervisor y notifiaciones
+//const URLU = 'https://serviciodeusuariosyubicaciones-ejc7emcfe4atbvde.canadacentral-01.azurewebsites.net/';
 
+// URL para microservicio de supervisor y notificaciones
+// URLU para microservicio de usuarios y ubicación
 import axios from 'axios';
 
-// Metodo POST para supervisor y notificaiones
+// Método POST para supervisor y notificaciones
 export const POSTU = async (resource, data, token = "NONE") => {
     let headers = {
         headers: {
             "Accept": "application/json",
             "Content-Type": "application/json"
         }
-    }
+    };
     if (token !== "NONE") {
         headers.headers["Authorization"] = `Bearer ${token}`;
     }
     return await axios.post(URLU + resource, data, headers);
-}
+};
 
-
-// Metodo GET para supervisor y notificaiones
+// Método GET para supervisor y notificaciones
 export const GETU = async (resource, token = "NONE") => {
     let headers = {
         headers: {
-            "Accept": "application/json",
+            "Accept": "application/json"
         }
-    }
+    };
     if (token !== "NONE") {
         headers.headers["Authorization"] = `Bearer ${token}`;
     }
     return await axios.get(URLU + resource, headers);
-}
-// Metodo POST
+};
+
+// Método POST para usuario y ubicación
 export const POST = async (resource, data, token = "NONE") => {
     let headers = {
         headers: {
             "Accept": "application/json",
             "Content-Type": "application/json"
         }
-    }
+    };
     if (token !== "NONE") {
         headers.headers["Authorization"] = `Bearer ${token}`;
     }
     return await axios.post(URL + resource, data, headers);
-}
+};
 
-// Metodo GET
+// Método GET para usuario y ubicación
 export const GET = async (resource, token = "NONE") => {
     let headers = {
         headers: {
-            "Accept": "application/json",
+            "Accept": "application/json"
         }
-    }
+    };
     if (token !== "NONE") {
         headers.headers["Authorization"] = `Bearer ${token}`;
     }
     return await axios.get(URL + resource, headers);
-}
-
-// Metodo DELETE
-export const DELETE = async (resource, token = "NONE") => {
-    let headers = {
-        headers: {
-            "Accept": "application/json",
-        }
-    }
-    if (token !== "NONE") {
-        headers.headers["Authorization"] = `Bearer ${token}`;
-    }
-    return await axios.delete(URL + resource, headers);
-}
+};
